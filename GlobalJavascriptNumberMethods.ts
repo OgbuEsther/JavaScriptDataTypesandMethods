be6e84e266be54aca061db10d1f9657bf0be463f
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-07 23:14:28 -0800

    saved changes

--- GlobalJavascriptNumberMethods.ts
+++ GlobalJavascriptNumberMethods.ts
@@ -14,3 +14,22 @@ console.log(Number("esther")) //output :NaN
 
 //If the number cannot be converted, NaN (Not a Number) is returned.
 
+//The Number() Method Used on Dates
+// Number() can also convert a date to a number.
+
+//example
+console.log(Number(new Date("2023-02-07"))) //The Date() method returns the number of milliseconds since 2023-02-07
+
+
+//the second global javascript number method is the parseInt() Method
+/*parseInt() parses a string and returns a whole number. Spaces are allowed. Only the first number is returned: */
+
+console.log(parseInt("-20")) //output : 20
+console.log(parseInt("years 30")) //output : NaN because the string value came before the number
+console.log(parseInt(" 20.33 ")) //output : 20 it wont look at the decimals
+console.log(parseInt("40years  ")) //output : 40
+
+
+///the third global javascript number method is the parseFloat() Method
+
+
