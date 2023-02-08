6f1f37d8bc17ad59bfbe32a468de063739acd56a
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-07 23:40:25 -0800

    saved changes

---
+++ NODECORES/process.ts
@@ -0,0 +1,10 @@
+//Process
+
+/*It is a global object and provides information related to the program execution. You do not need to load it using require() method.
+What are the NodeJS process properties?
+Image result for process in node js
+Node. js provides the facility to get process information such as process id, architecture, platform, version, release, uptime, upu usage etc. It can also be used to kill process, set uid, set groups, unmask etc. The process is a global object, an instance of EventEmitter, can be accessed from anywhere.
+
+*/
+
+
