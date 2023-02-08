3cf9ec5d458b68c5bbf5fbbfa355429cee4805f1
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-07 23:05:17 -0800

    saved changes

---
+++ GlobalJavascriptNumberMethods.ts
@@ -0,0 +1,6 @@
+//covverting variables to Numbers
+
+/*there are 3 javasript methods that can be used to convert a variable to a number and they are not number methods but javascript global methods */
+
+//Number() :Returns a number converted from its argument.
+
